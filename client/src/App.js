f2d983735dac497aaf8dde8c2cbf764c2e44c6bd
greemaltend4 <dengestabiy@hotmail.com>
2025-11-06 12:10:25 +0000

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue dans le Gestionnaire de Données Intégré au Cloud !</h1>
+    </div>
+  );
+}
+
+export default App;
